8c5079a8d1c6ff0ac23f03352a172ba36818f5f5
Timur-Malik <92898560+tim-fihost@users.noreply.github.com>
2024-02-26 06:32:18 +0900

    Update forms.py

--- forms.py
+++ forms.py
@@ -13,20 +13,20 @@ class CreatePostForm(FlaskForm):
     submit = SubmitField("Submit Post")
 
 
-# TODO: Create a RegisterForm to register new users
+#Create a RegisterForm to register new users
 class CreateRegisterForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired()]) #check if @ exists
     name = StringField("Name", validators=[DataRequired()])
     passwrod = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("SIGN ME UP")
 
-# TODO: Create a LoginForm to login existing users
+# Create a LoginForm to login existing users
 class CreateLoginForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Login")
 
-# TODO: Create a CommentForm so users can leave comments below posts
+#Create a CommentForm so users can leave comments below posts
 class CreateCommentForm(FlaskForm):
     comment = CKEditorField("Comment",validators=[DataRequired()])
     sbumit = SubmitField("Comment")
